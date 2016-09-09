aed7d6628a25c2d78ef807704dc1d56079777b06
Olivier Lalonde <olalonde@gmail.com>
2016-09-09 10:40:04 -0700

    implement atomic mode

--- test.js
+++ test.js
@@ -4,16 +4,52 @@ var fs = require('./')
 var os = require('os')
 var path = require('path')
 var rimraf = require('rimraf')
+var Readable = require('stream').Readable
 
-var common = {
-  setup: function(t, cb) {
-    // make a new blobs instance on every test
-    cb(null, fs(path.join(os.tmpdir(), ''+process.pid)))
-  },
-  teardown: function(t, store, blob, cb) {
-    rimraf.sync(store.path)
-    cb()
+var common = function (atomic) {
+  return {
+    setup: function(t, cb) {
+      const opts = {
+        path: path.join(os.tmpdir(), ''+process.pid),
+        atomic: atomic,
+      }
+      // make a new blobs instance on every test
+      cb(null, fs(opts))
+    },
+    teardown: function(t, store, blob, cb) {
+      rimraf.sync(store.path)
+      cb()
+    }
   }
 }
 
-tests(tape, common)
+tests(tape, common())
+
+tests(tape, common(true))
+
+tape(function (t) {
+  var store = fs({
+    path: path.join(os.tmpdir(), ''+process.pid),
+    atomic: true,
+  })
+  rimraf.sync(store.path)
+
+  var i = 0
+  var brokenReadStream = new Readable()
+  brokenReadStream._read = function () {
+    i++
+    if (i == 3) {
+      this.emit(new Error('simulated error'))
+    }
+  }
+
+  brokenReadStream.pipe(store.createWriteStream('somekey'))
+
+  setTimeout(function () {
+    store.exists('somekey', function (err, exists) {
+      if (err) return t.fail(err)
+      t.equal(exists, false)
+      t.end()
+    })
+  }, 200)
+})
